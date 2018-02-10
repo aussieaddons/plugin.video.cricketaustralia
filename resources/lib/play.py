c77f614606d78fe108afdc4deac0bd6d7f09034a
Andy Botting <andy@andybotting.com>
2018-02-10 21:24:58 +1100

    Hack to use STRM instead

--- resources/lib/play.py
+++ resources/lib/play.py
@@ -1,5 +1,6 @@
 import comm
 import config
+import os
 import sys
 import xbmc
 import xbmcgui
@@ -9,6 +10,7 @@ from aussieaddonscommon import utils
 
 pluginhandle = int(sys.argv[1])
 
+
 def play(params):
     try:
         success = True
@@ -17,13 +19,22 @@ def play(params):
         utils.log('Attempting to play: {0} {1}'.format(stream['name'],
                                                        stream['url']))
 
-        listitem = xbmcgui.ListItem(path=stream['url'])
-        listitem.setProperty('inputstreamaddon', 'inputstream.adaptive')
-        listitem.setProperty('inputstream.adaptive.manifest_type', 'hls')
-        listitem.setMimeType('application/vnd.apple.mpegurl')
-        listitem.setContentLookup(False)
+        # TODO(andy): Fix strm file hack
+        strm = os.path.join(utils.get_file_dir(), 'stream.strm')
+        with open(strm, 'w') as f:
+            f.write('#KODIPROP:inputstreamaddon=inputstream.adaptive\n'
+                    '#KODIPROP:inputstream.adaptive.manifest_type=hls\n'
+                    '{0}'.format(stream['url']))
 
-        xbmcplugin.setResolvedUrl(pluginhandle, True, listitem=listitem)
+        item = xbmcgui.ListItem(path=stream['url'])
+        xbmc.Player().play(item)
+
+        #item = xbmcgui.ListItem(path=stream['url'])
+        #item.setProperty('inputstreamaddon', 'inputstream.adaptive')
+        #item.setProperty('inputstream.adaptive.manifest_type', 'hls')
+        #item.setMimeType('application/vnd.apple.mpegurl')
+        #item.setContentLookup(False)
+        #xbmcplugin.setResolvedUrl(pluginhandle, True, item=item)
     except Exception:
         utils.handle_error('Unable to play video')
 

cdc5441e869572f15d15bfd5ba55eb20f17db2a6
Pointybirds <dan@noshutsherlock.net>
2020-10-10 13:26:17 +1000

    Updated API URL for livestreams

--- resources/lib/config.py
+++ resources/lib/config.py
@@ -5,6 +5,8 @@ VIDEOS_URL = 'https://apinew.cricket.com.au/videos?Limit=30'
 
 MATCHES_URL = 'https://apinew.cricket.com.au/matches?inProgressLimit=15&completedLimit=0&upcomingLimit=0&format=json'
 
+LIVETV_URL = 'https://apinew.cricket.com.au/matches/livetv'
+
 MATCH_STREAM_URL = 'https://edge.api.brightcove.com/playback/v1/accounts/807051129001/videos/'
 
 BRIGHTCOVE_PK = 'BCpkADawqM17uKWqEHlBulux385QZ_BoC6x04LRDmsykNRb4uwwRJ8x38iHNk-7kxEqJUu3qZGMFCiKA4d8SeUB0c40Z46CutsbR219abTqUHi82DqCZMUJo36s'
